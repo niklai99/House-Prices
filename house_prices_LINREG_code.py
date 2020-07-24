b85cad8d249ad35fbe1864b0c9c14fa157a430eb
Nik <62773963+niklai99@users.noreply.github.com>
2020-07-24 13:38:27 +0200

    Started working on linear regression

---
+++ house_prices_LINREG_code.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV, ElasticNetCV
+from sklearn.metrics import mean_squared_error, make_scorer
+from scipy.stats import skew
+from IPython.display import display
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def get_data():
+    file = 'data/train.csv'
+    df_train = pd.read_csv(file)
+    return df_train
